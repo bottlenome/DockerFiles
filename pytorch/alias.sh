10b36aec233aeec0f7b818244e91c4734085d667
bottlenome <bottlenome@gmail.com>
2020-07-18 04:10:27 +0900

    Add jupyter alias

--- pytorch/alias.sh
+++ pytorch/alias.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 alias pytorch=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run.sh $PWD python'
+alias pytorch.jupyter=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run_port.sh $PWD jupyter notebook --ip=*'
