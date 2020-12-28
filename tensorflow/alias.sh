3526fa0d41775306084cd8018c8b9336906d6d0b
bottlenome <bottlenome@gmail.com>
2020-12-28 15:49:07 +0900

    Add tensorflow environment

---
+++ tensorflow/alias.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+alias tensorflow=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run.sh $PWD python3'
---
+++ tensorflow/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+EXEC_PATH=${1:-/home/$USER}
+COMMAND=${@:2:($#-1)}
+
+docker run -it --gpus all \
+               -w ${EXEC_PATH} \
+               -e DISPLAY=$DISPLAY \
+               -v /tmp/.X11-unix:/tmp/.X11-unix \
+               -v /etc/group:/etc/group:ro \
+               -v /etc/passwd:/etc/passwd:ro \
+               -v /home/$USER/:/home/$USER \
+               -v /mnt:/mnt \
+               -u $(id -u $USER):$(id -g $USER) bottlenome/tensorflow:latest ${COMMAND}
