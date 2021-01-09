3cc29ab4a96e5f2f71c7e4d4797e815f5623fd12
bottlenome <bottlenome@gmail.com>
2021-01-09 16:02:02 +0900

    Add gpt2 docker

---
+++ gpt2/alias.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+alias gpt2=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run.sh $PWD python3'
---
+++ gpt2/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+EXEC_PATH=${1:-/home/$USER}
+COMMAND=${@:2:($#-1)}
+
+docker run -it --gpus all \
+               -w ${EXEC_PATH} \
+               -v /etc/group:/etc/group:ro \
+               -v /etc/passwd:/etc/passwd:ro \
+               -v /home/$USER/:/home/$USER \
+               -v /mnt:/mnt \
+               -u $(id -u $USER):$(id -g $USER) bottlenome/gpt2:latest ${COMMAND}
