8937298ee2bc1fc5827a037660bdc29d3b9b4b24
bottlenome <bottlenome@gmail.com>
2023-11-20 18:32:52 +0900

    Save latest

--- pytorch/alias.sh
+++ pytorch/alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-alias pytorch=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run.sh $PWD python'
-alias pytorch.jupyter=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run_port.sh $PWD jupyter notebook --ip=*'
-alias pytorch.tensorboard=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run_port.sh $PWD tensorboard --port 8888 --bind_all --logdir=./runs'
+alias pytorch=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run.sh $PWD python'
+alias pytorch.jupyter=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run_port.sh $PWD jupyter notebook --ip=*'
+alias pytorch.tensorboard=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run_port.sh $PWD tensorboard --port 8888 --bind_all --logdir=./runs'
--- pytorch18/alias.sh
+++ pytorch18/alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-alias pytorch18=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run.sh $PWD python'
-alias pytorch18.jupyter=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run_port.sh $PWD jupyter notebook --ip=*'
-alias pytorch18.tensorboard=$(cd $(dirname ${BASH_SOURCE:$0}); pwd)'/run_port.sh $PWD tensorboard --port 8888 --bind_all --logdir=./runs'
+alias pytorch18=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run.sh $PWD python'
+alias pytorch18.jupyter=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run_port.sh $PWD jupyter notebook --ip=*'
+alias pytorch18.tensorboard=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run_port.sh $PWD tensorboard --port 8888 --bind_all --logdir=./runs'
+alias pytorch18.cmd=$(cd $(dirname ${BASH_SOURCE}); pwd)'/run.sh $PWD'
